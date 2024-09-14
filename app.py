4ae19c6d03833e650a623e2b39b7bbaad1236d72
simtheverse <110426028+simtheverse@users.noreply.github.com>
2024-09-14 22:54:11 +0000

    Make websocket so all users get updated

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_socketio import SocketIO, emit
+import logging
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 people = []
 
@@ -16,7 +19,15 @@ def get_people():
 def add_person():
     person_data = request.json
     people.append(person_data)
+    logging.info(f'Emitting update_people event with data: {people}')
+    socketio.emit('update_people', people)
     return jsonify({'status': 'success', 'person': person_data})
 
+@socketio.on('connect')
+def handle_connect():
+    logging.info('Client connected')
+    emit('update_people', people)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    logging.basicConfig(level=logging.INFO)
+    socketio.run(app, debug=True)
